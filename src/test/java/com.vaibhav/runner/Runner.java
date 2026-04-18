cbd688f7b1b0fe6fec3f2a8a273faf7e012ead21
Caibhav.Gupta.ME <vaibhav.gupta@mamaearth.in>
2026-04-18 20:39:21 +0530

    Runner file added

---
+++ src/test/java/com.vaibhav/runner/Runner.java
@@ -0,0 +1,18 @@
+package com.vaibhav.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/com.vaibhav.features", // Placeholder
+        glue = {"com.vaibhav.stepDefinition", "com.vaibhav.hooks"},
+//        plugin = {"pretty", "com.vaibhav.tools.StepLoggerPlugin"},// Placeholder
+        tags = "not @wip",
+        plugin = {"com.vaibhav.tools.StepLoggerPlugin"},// Placeholder
+        monochrome = true
+)
+public class Runner {
+}
